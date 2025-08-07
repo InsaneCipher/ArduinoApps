df9e35e8683f28d1642cb380370b8ddd20bbedc9
Insane <123465584+InsaneCipher@users.noreply.github.com>
2025-08-07 12:00:42 +0200

    Update SpaceShooter.ino

--- SpaceShooter/SpaceShooter.ino
+++ SpaceShooter/SpaceShooter.ino
@@ -24,7 +24,7 @@
 #define DEADZONE 100        // Joystick deadzone
 #define BLINK_DELAY 500   // Delay between blinks
 #define MOVE_INTERVAL 100
-#define RELOAD_TIME 500
+#define RELOAD_TIME 300
 
 // ========== Objects ==========
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
@@ -33,16 +33,18 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 // ========== Globals ==========
 int xValue = 0;
 int checkJoy = 0;
-uint8_t brightness = 10;
+uint8_t brightness = 20;
 unsigned long currentTime;
 unsigned long lastMoveTime = 0;
 unsigned long lastShootMoveTime = 0;
 unsigned long lastShotTime = 0;
 unsigned long lastEnemyShotTime = 0;
-unsigned long enemyShootDelay = 500;
-int score = 0;
+int baseEnemyShootDelay = 1000;
+int enemyShootDelay = baseEnemyShootDelay;
+unsigned long score = 0;
 int enemies = 0;
 int kills = 0;
+int wave = 0;
 
 // Cursor position
 uint8_t cursorX = 4;
@@ -53,7 +55,8 @@ const uint8_t ENEMY_BULLET = 2;
 const uint8_t ENEMY_A = 3;
 const uint8_t ENEMY_B = 4;
 const uint8_t ENEMY_C = 5;
-const uint8_t CURSOR = 6;
+const uint8_t ENEMY_D = 6;
+const uint8_t CURSOR = 7;
 
 uint8_t blinks = 0;
 
@@ -64,17 +67,18 @@ uint8_t grid[8][8];
 const int A = ENEMY_A;
 const int B = ENEMY_B;
 const int C = ENEMY_C;
+const int D = ENEMY_D;
 
 const uint8_t WAVE_1[3][8] = {
-  {A,A,A,A,A,A,A,A},
-  {A,0,A,0,A,0,A,0},
-  {C,C,C,A,A,C,C,C}
+  {C,C,C,C,C,C,C,C},
+  {A,0,0,A,A,0,0,A},
+  {C,A,A,C,C,A,A,C}
 };
 
 const uint8_t WAVE_2[3][8] = {
   {A,0,A,0,A,0,A,0},
-  {A,0,A,0,A,0,A,0},
-  {C,0,C,0,C,0,C,0}
+  {C,0,C,0,C,0,C,0},
+  {A,0,C,0,C,0,A,0}
 };
 
 const uint8_t WAVE_3[3][8] = {
@@ -83,11 +87,60 @@ const uint8_t WAVE_3[3][8] = {
   {B,B,B,B,B,B,B,B}
 };
 
+const uint8_t WAVE_4[3][8] = {
+  {B,C,B,0,0,B,C,B},
+  {D,A,D,0,0,D,A,D},
+  {A,B,A,0,0,A,B,A}
+};
+
+const uint8_t WAVE_5[3][8] = {
+  {C,A,C,0,0,C,A,C},
+  {A,D,A,B,B,A,D,A},
+  {0,0,0,0,0,0,0,0}
+};
+
+const uint8_t WAVE_6[3][8] = {
+  {0,A,D,D,D,D,A,0},
+  {0,0,A,D,D,A,0,0},
+  {0,0,0,A,A,0,0,0}
+};
+
+const uint8_t WAVE_7[3][8] = {
+  {A,B,A,B,B,A,B,A},
+  {B,A,B,A,A,B,A,B},
+  {A,B,A,B,B,A,B,A}
+};
+
+const uint8_t WAVE_8[3][8] = {
+  {C,0,0,0,0,0,0,C},
+  {B,C,0,0,0,0,C,B},
+  {B,B,C,0,0,C,B,B}
+};
+
+const uint8_t WAVE_9[3][8] = {
+  {D,A,D,A,D,A,D,A},
+  {A,A,A,A,A,A,A,A},
+  {0,0,0,0,0,0,0,0}
+};
+
+const uint8_t WAVE_10[3][8] = {
+  {B,B,B,B,B,B,B,B},
+  {A,A,A,A,A,A,A,A},
+  {C,B,C,B,C,B,C,B}
+};
+
 
 const uint8_t (*WAVES[])[8] = {
   WAVE_1,
   WAVE_2,
-  WAVE_3
+  WAVE_3,
+  WAVE_4,
+  WAVE_5,
+  WAVE_6,
+  WAVE_7,
+  WAVE_8,
+  WAVE_9,
+  WAVE_10
 };
 
 const byte NUM_WAVES = sizeof(WAVES) / sizeof(WAVES[0]);
@@ -107,8 +160,17 @@ void initGame() {
   startLCD();
   strip.show();
   score = 0;
+  wave = 0;
   cursorX = 4;
   updateLeds();
+}
+
+void freeMemory() {
+  extern int __heap_start, *__brkval;
+  int v;
+  int freeMem = (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+  Serial.print("Free memory: ");
+  Serial.println(freeMem);
 }
 
 // ========== Setup Functions ==========
@@ -142,15 +204,19 @@ void updateLeds() {
       }
 
       if (grid[row][col] == ENEMY_B) {
-        led(row, col, brightness, 0, brightness);
-      }
-
-      if (grid[row][col] == ENEMY_C) {
         led(row, col, 0, 0, brightness);
       }
 
+      if (grid[row][col] == ENEMY_C) {
+        led(row, col, brightness, 0, brightness);
+      }
+
+      if (grid[row][col] == ENEMY_D) {
+        led(row, col, brightness*3, brightness/2.5, 0);
+      }
+
       if (grid[row][col] == ENEMY_BULLET) {
-        led(row, col, brightness, 0, 0);
+        led(row, col, brightness*4, 0, 0);
       }
 
       // Draw Cursor
@@ -174,7 +240,20 @@ void updateLCD() {
   lcd.setCursor(0, 0);
   lcd.print("Score: " + String(score));
   lcd.setCursor(0, 1);
-  lcd.print("Kills: " + String(kills) + "/" + String(enemies));
+  String whitespace = "    ";
+  if (wave < 10) {
+    whitespace += " ";
+  }
+
+  if (kills < 100) {
+    if (kills < 10) {
+      whitespace += " ";
+    }
+    whitespace += " ";
+  }
+  
+  // lcd.print("K: " + String(kills) + whitespace + "W: " + String(wave));
+  lcd.print("Wave: " + String(wave) + whitespace + String(kills) + "K");
 }
 
 void gameoverLCD() {
@@ -182,9 +261,17 @@ void gameoverLCD() {
   lcd.setCursor(0, 0);
   lcd.print("Gameover!");
   lcd.setCursor(0, 1);
-  lcd.print("Score: " + String(score));
-  Serial.println("Score: " + String(score));
+  lcd.print("Died Wave: " + String(wave));
   delay(2000);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Kills: " + String(kills) + "/" + String(enemies));
+  lcd.setCursor(0, 1);
+  lcd.print("Score: " + String(score));
+  delay(2000);
+
+  Serial.println("Score: " + String(score));
   initGame();
 }
 
@@ -211,13 +298,20 @@ void setupTargets() {
   for (int row = 0; row < 3; row++) {
     for (int col = 0; col < 8; col++) {
       if (wave[row][col] != 0) {
-        enemies++;
-        updateLCD();
+        if (wave[row][col] == ENEMY_C) {
+          enemies += 2;
+        } else if (wave[row][col] == ENEMY_D) {
+          enemies += 3;
+        } else {
+          enemies++;
+        }
       }
 
       grid[7 - row][col] = wave[row][col];
     }
   }
+
+  updateLCD();
 }
 
 
@@ -239,27 +333,43 @@ void advanceBullet() {
             if (grid[row + 1][col] == EMPTY) {
               grid[row + 1][col] = BULLET;
               grid[row][col] = EMPTY;
-            } else if (abovePos == ENEMY_A || abovePos == ENEMY_B || abovePos == ENEMY_C) {
+            } else if (abovePos == ENEMY_A || abovePos == ENEMY_B || abovePos == ENEMY_C || abovePos == ENEMY_D) {
               // Add score
               if (abovePos == ENEMY_A) {
-                score += 10;
+                score += 10*wave;
+
+                // Destroy enemy and bullet
+                grid[row + 1][col] = EMPTY;
+                grid[row][col] = EMPTY;
               }
               if (abovePos == ENEMY_B) {
-                score += 20;
+                score += 20*wave;
+
+                // Destroy enemy and bullet
+                grid[row + 1][col] = EMPTY;
+                grid[row][col] = EMPTY;
               }
               if (abovePos == ENEMY_C) {
-                score += 50;
-              }
+                score += 50*wave;
 
-              // Destroy enemy and bullet
-              grid[row + 1][col] = EMPTY;
-              grid[row][col] = EMPTY;
+                // Destroy enemy and bullet
+                grid[row + 1][col] = ENEMY_B;
+                grid[row][col] = EMPTY;
+              }
+              if (abovePos == ENEMY_D) {
+                score += 100*wave;
+
+                // Destroy enemy and bullet
+                grid[row + 1][col] = ENEMY_C;
+                grid[row][col] = EMPTY;
+              }
               kills++;
 
               updateLCD();
             } else if (abovePos == ENEMY_BULLET) {
               grid[row + 1][col] = EMPTY;
               grid[row][col] = EMPTY;
+              score += 1*wave;
             }
           } else {
             // Remove bullet if it reached the top
@@ -278,7 +388,6 @@ void advanceBullet() {
             grid[row][col] = EMPTY;
           } else if ((row - 1 == 0 && col == cursorX) || (row == 0 && col == cursorX)) {
             gameover();
-            initGame();
           } else {
             grid[row][col] = EMPTY; // Hit something or reached bottom
           }
@@ -311,25 +420,19 @@ void enemyShoot() {
         uint8_t slot = grid[row][col];
         uint8_t fireChance = random(100);
 
-        if (fireChance >= 80) {
-          if ((slot == ENEMY_B || slot == ENEMY_C) && !hasEnemyBelow(row, col)) {
+        if (fireChance >= 50) {
+          if ((slot == ENEMY_B || slot == ENEMY_C || slot == ENEMY_D) && !hasEnemyBelow(row, col)) {
             if (row > 0 && grid[row - 1][col] == EMPTY) {
-              Serial.print("Enemy tries to shoot at: ");
-              Serial.print(row);
-              Serial.print(", ");
-              Serial.println(col);
               grid[row - 1][col] = ENEMY_BULLET;
-              lastEnemyShotTime = currentTime;
-              return;
             }
           }
         }
       }
     }
-
+    lastEnemyShotTime = currentTime;
   }
-
 }
+
 
 // ========== Game Logic ==========
 
@@ -358,6 +461,16 @@ void loop() {
 
   if (kills >= enemies) {
     setupTargets();
+    wave++;
+
+    if (wave > 99) {
+      gameover();
+    }
+
+    enemyShootDelay = baseEnemyShootDelay -= (50*wave);
+    if (enemyShootDelay < 200) {
+      enemyShootDelay = 200;
+    }
   }
 
   // Handle cursor movement
